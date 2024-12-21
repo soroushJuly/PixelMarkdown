7aab206323b203ea0b73a14584dbc9585cfe9d8a
Soroush <soroushdevelope@gmail.com>
2024-12-21 22:57:14 +0000

    feat: add project colors

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -12,6 +12,19 @@ export default defineNuxtPlugin((app) => {
     ssr: true,
     components,
     directives,
+    theme: {
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: "#333D45", // #333D45
+            secondary: "#999EA2", // #999EA2
+            placeholder: "#C2C5C7", // #C2C5C7
+            borderColor: "#E0E2E3", // #E0E2E3
+          }
+        },
+      },
+    },
   })
   app.vueApp.use(vuetify)
 })
