f807d1302d09940db9de93cd1f14c8b7c1d51d7c
Soroush <soroushdevelope@gmail.com>
2024-12-22 21:02:40 +0000

    update: default markdown

--- public/default-markdown.js
+++ public/default-markdown.js
@@ -12,9 +12,13 @@ Lorem ipsum sit amet.
 
 This is a [link](https://exemplar.com)
 
-[img=”https://exemplar.com/image.jpg”]
+[file="https://drive.google.com/file/d/1h45mtGM_YbFuhxE0bdz-T7eS8iMi4K3S/view?usp=drive_link"]
 
-[col-left][img=”https://exemplar.com/image.jpg”][/col-left][col-right][img=”https://exemplar.com/image.jpg”][/col-right]
+[video="https://media.istockphoto.com/id/1158647615/video/close-up-view-of-unrecognisable-female-customer-choosing-a-color-sample-at-a-paint-shop.mp4?s=mp4-640x640-is&k=20&c=OO3UW6frNcHdZvy3unpsfpgh1nzLR6GbrZ7JVw62OqI="]
+
+[img="https://cdn.pixelfield.co.uk/_assets/img/landing-page/card-logo-1.svg"]
+
+[col-left][img="https://cdn.pixelfield.co.uk/_assets/img/landing-page/card-logo-1.svg"][/col-left][col-right][img="https://cdn.pixelfield.co.uk/_assets/img/landing-page/card-logo-1.svg"][/col-right]
 
 [table][row][col-left]**Make it bold**[/col-left][col-right]**Make it bold**[/col-right][/row][row][col-left]Content in the left column[/col-left][col-right]Content in the right column[/col-right][/row][row][col-left]Content in the left column[/col-left][col-right]Content in the right column[/col-right][/row][/table]`
 
