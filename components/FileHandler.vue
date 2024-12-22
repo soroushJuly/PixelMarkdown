bdace284552e5161cb4085faee44e9bf6ead6c42
Soroush <soroushdevelope@gmail.com>
2024-12-22 21:31:45 +0000

    refactor: separate file handler and modal

---
+++ components/FileHandler.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-list>
+    <v-list-item
+      v-for="(type, i) in uploadTypes"
+      :key="'file type-'+i"
+    >
+      <p>
+        Add {{ type.name }}:
+      </p>
+      <v-file-input
+        multiple
+        :label="`${type.name} Upload`"
+        :accept="type.inputFromat"
+        @update:model-value="handleFile"
+      />
+      <v-btn @click="AddMarkup(type.markup(selectedFile))">
+        Add
+      </v-btn>
+    </v-list-item>
+  </v-list>
+  <v-list>
+    <v-list-item
+      v-for="(item, i) in fileList"
+      :key="'file-'+i"
+    >
+      {{ item.name }}
+    </v-list-item>
+  </v-list>
+</template>
+
+<script setup>
+import fileUploadTypes from '~/public/file-upload-types'
+
+// TODO: will be used to implement adding with URL of the files
+const selectedFile = ref('')
+const fileList = reactive([])
+const uploadTypes = computed(() => fileUploadTypes)
+
+// Prepare and send the files to server and wait for results
+const handleFile = async (params) => {
+  for (let i = 0; i < params.length; i++) {
+    const file = await uploadFile(params[i])
+    fileList.push(file)
+  }
+}
+
+// Uploading simulation
+const uploadFile = (file) => {
+  return new Promise((resolve, reject) => {
+    // Emulate server upload
+    setTimeout(() => {
+      resolve({
+        url: `https://examplar.com/${file.name}`,
+        type: file.type,
+        size: file.size,
+        name: file.name,
+      })
+    }, 2000)
+  },
+  )
+}
+
+// TODO: using state manager could have been better
+const AddMarkup = (markup) => {
+  emit('update:content', markup)
+  closeModal()
+}
+</script>
--- components/ModalMediaMarkup.vue
+++ components/ModalMediaMarkup.vue
@@ -11,33 +11,7 @@
         List of available options:
       </v-card-item>
       <v-card-text>
-        <v-list>
-          <v-list-item
-            v-for="(type, i) in uploadTypes"
-            :key="'file type-'+i"
-          >
-            <p>
-              Add {{ type.name }}:
-            </p>
-            <v-file-input
-              multiple
-              :label="`${type.name} Upload`"
-              :accept="type.inputFromat"
-              @update:model-value="handleFile"
-            />
-            <v-btn @click="AddMarkup(type.markup(selectedFile))">
-              Add
-            </v-btn>
-          </v-list-item>
-        </v-list>
-        <v-list>
-          <v-list-item
-            v-for="(item, i) in fileList"
-            :key="'file-'+i"
-          >
-            {{ item.name }}
-          </v-list-item>
-        </v-list>
+        <FileHandler @update:content="AddMarkup" />
       </v-card-text>
       <template #actions>
         <v-btn
@@ -51,51 +25,18 @@
 </template>
 
 <script setup>
-import { defineProps, defineEmits, watch, ref, reactive } from 'vue'
-
-const uploadTypes = [
-  { name: 'image', inputFromat: 'image/*', markup: url => `\n[img="${url}"]` },
-  { name: 'video', inputFromat: '', markup: url => `\n[video="${url}"]` },
-  { name: 'file', inputFromat: '', markup: url => `\n[file="${url}"]` },
-]
-
-// TODO: will be used to implement adding with URL of the files
-const selectedFile = ref('')
-const fileList = reactive([])
-
-// Uploading simulation
-const uploadFile = (file) => {
-  return new Promise((resolve, reject) => {
-    // Emulate server upload
-    setTimeout(() => {
-      resolve({
-        url: `https://examplar.com/${file.name}`,
-        type: file.type,
-        size: file.size,
-        name: file.name,
-      })
-    }, 2000)
-  },
-  )
-}
-
-const handleFile = async (params) => {
-  for (let i = 0; i < params.length; i++) {
-    const file = await uploadFile(params[i])
-    fileList.push(file)
-  }
-}
-
-const AddMarkup = (markup) => {
-  emit('update:content', markup)
-  closeModal()
-}
+import { defineProps, defineEmits, watch, ref } from 'vue'
 
 const props = defineProps({
   show: Boolean,
 })
 
 const emit = defineEmits(['update:show', 'update:content'])
+
+const AddMarkup = (markup) => {
+  emit('update:content', markup)
+  closeModal()
+}
 
 // Local copy of show prop
 const localShow = ref(props.show)
