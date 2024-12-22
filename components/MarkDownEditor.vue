a962eff2e660c4079b2454ab4ba023dad674e90e
Soroush <soroushdevelope@gmail.com>
2024-12-22 21:10:16 +0000

    feat: update editor and renderer

--- components/MarkDownEditor.vue
+++ components/MarkDownEditor.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="border-sm text-secondary rounded px-4">
+    <ModalMediaMarkup
+      v-model:show="isModalVisible"
+      @update:content="AppendContent"
+    />
     <div class="py-2">
-      headerd
+      <button
+        v-for="(control, i) in controlsList"
+        :key="'control-'+i"
+        class="pr-4"
+        @click="handleControl(control.action)"
+      >
+        {{ control.name }}
+      </button>
     </div>
     <v-divider />
     <div class="text-placeholder pb-4">
-      <v-container class="pa-0 text-primary" fluid>
+      <v-container
+        class="pa-0 text-primary"
+        fluid
+      >
         <v-textarea
           v-model="markdownContent"
           placeholder="Placeholder"
           variant="plain"
           rows="28"
-          style="style"
           @update:model-value="handleChange"
         />
       </v-container>
@@ -21,17 +34,43 @@
 
 <script setup>
 import { ref, defineEmits } from 'vue'
-import sampleMarkdown from '~/public/default-markdown';
+import sampleMarkdown from '~/public/default-markdown'
+import CreateSpecialTags from '~/public/tools/create-special-tags'
 
 const emit = defineEmits(['newMarkdown'])
-
 const markdownContent = ref(sampleMarkdown)
+const isModalVisible = ref(false)
 
-const handleChange = ((event) => {
-  emit('newMarkdown', event)
-})
+const handleChange = (event) => {
+  const specializedMarkdown = CreateSpecialTags(event)
+  emit('newMarkdown', specializedMarkdown)
+}
+
+// Add new content at the end of the markdown content
+const AppendContent = (content) => {
+  markdownContent.value += content
+  handleChange(markdownContent.value)
+}
 
 // Render the sample on setup (initialization)
 handleChange(markdownContent.value)
-</script>
 
+const handleControl = (action) => {
+  if (action) {
+    action()
+  }
+}
+
+const openModal = () => {
+  isModalVisible.value = true
+}
+const controlsList = [
+  { name: 'Image', action: () => { openModal() } },
+  { name: 'Video', action: () => { openModal() } },
+  { name: 'Link', action: () => '' },
+  { name: 'File', action: () => { openModal() } },
+  { name: '2 columns', action: () => '' },
+  { name: 'Table', action: () => '' },
+  { name: 'Table Row', action: () => '' },
+]
+</script>
--- components/MarkDownRenderer.vue
+++ components/MarkDownRenderer.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <h3 class="text-h6	text-primary pb-5">Rendered HTML</h3>
-        <div v-html="markdown.render(props.content)" />
-    </div>
+  <div>
+    <h3 class="text-h6	text-primary pb-5">
+      Rendered HTML
+    </h3>
+    <div v-html="renderedMarkdown" />
+  </div>
 </template>
 
 <script setup>
-import MarkdownIt from 'markdown-it'
-import { defineProps } from 'vue';
+import { computed } from 'vue'
+import markdownit from 'markdown-it'
+import markdownItConfig from '~/public/tools/markdown-it-config';
 
 const props = defineProps(['content'])
-const markdown = new MarkdownIt()
+const md = markdownit(markdownItConfig)
 
+const renderedMarkdown = computed(() => {
+  return md.render(props.content)
+})
 </script>
